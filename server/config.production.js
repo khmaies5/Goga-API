8b39a81646f53c14565776cc7eea5b2c0b579787
khmaieshassen <hssen.khmaies@esprit.tn>
2018-05-08 23:48:11 +0100

    add config

---
+++ server/config.production.js
@@ -0,0 +1,13 @@
+// Copyright IBM Corp. 2014,2015. All Rights Reserved.
+// Node module: loopback-example-offline-sync
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+'use strict';
+
+
+
+module.exports = {
+    host: process.env.CUSTOM_HOST,
+    port: process.env.CUSTOM_PORT
+};
